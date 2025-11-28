254ca83c6fe9bcedc7e7f0c42a781c30837838a7
Gokul Rajesh <gokulrajesh2024@gmail.com>
2025-11-29 02:04:41 +0530

    small change

--- src/test/java/testrunner.java
+++ src/test/java/testrunner.java
@@ -1,7 +1,9 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="features",glue= {"stepdefinition"})
+@CucumberOptions(features="features",glue= {"stepdefinition"},
+plugin = {"pretty", "html:testReports/cucumber-reports.html","io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"},
+monochrome = true)
 public class testrunner extends AbstractTestNGCucumberTests {
 	
 	
